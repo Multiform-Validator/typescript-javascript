928d0542089bebd96bcd1b96a51bac36b3a992c8
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-07-26 10:40:56 -0300

    bump: 2.2.1
    fix: 2.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const commonjsWebpackConfig = {
 	mode: "production", // production mode
 	entry: "./index.ts", // entry point of your application
 	output: {
-		filename: "cjs/index.js", // output file name
+		filename: "cjs/index.cjs", // output file name
 		path: path.resolve(__dirname, "dist"), // output folder
 		libraryTarget: "umd", // this allows your module to be used via require() and as a global
 		globalObject: "this", // this ensures that 'this' is 'window' in a browser environment
@@ -23,4 +23,29 @@ const commonjsWebpackConfig = {
 	},
 };
 
-module.exports = [commonjsWebpackConfig]; // export the webpack config
+const moduleWebpackConfig = {
+	mode: "production", // production mode
+	entry: "./index.ts", // entry point of your application
+	output: {
+		filename: "esm/index.mjs", // output file name
+		path: path.resolve(__dirname, "dist"), // output folder
+		libraryTarget: "module", // this allows your module to be used via import
+	},
+	experiments: {
+		outputModule: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts"],
+	},
+};
+
+module.exports = [commonjsWebpackConfig, moduleWebpackConfig]; // export the webpack config
